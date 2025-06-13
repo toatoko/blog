0aeef3b90ba43925acda400c8557a238b12af42d
toatoko <tokora212@gmail.com>
2025-06-13 20:43:04 +0300

    fixed url tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  extend FriendlyId
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :body, presence: true
   belongs_to :user
@@ -10,8 +11,14 @@ class Post < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     [ "rich_text_body", "title", "user_id", "created_at", "updated_at", "id", "views" ]
   end
-
+  
   def self.ransackable_associations(auth_object = nil)
     [ "user", "rich_text_body" ]
+  end
+  
+  friendly_id :title, use: %i[slugged history ]
+
+  def should_generate_new_friendly_id?
+    title_changed? || slug.blank?
   end
 end
