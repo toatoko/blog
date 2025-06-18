1347f3b56ea7411c6320f7fe14e5336b86230cd0
toatoko <tokora212@gmail.com>
2025-06-18 16:49:06 +0300

    for now

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,7 +16,7 @@ class Post < ApplicationRecord
     [ "user", "rich_text_body" ]
   end
   
-  friendly_id :title, use: %i[slugged history ]
+  friendly_id :title, use: %i[slugged history finders]
 
   def should_generate_new_friendly_id?
     title_changed? || slug.blank?
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,27 @@ User.create(email: 'test@test.com', name: "Toko", password: '123456', password_c
 User.create(email: 'baka@baka.com', name: "Baka", password: '123456', password_confirmation: '123456')
 
 
-10.times do |x|
-  post = Post.create(
+Toko = User.first
+Baka = User.second
+elapsed = Benchmark.measure do
+  posts = []
+  1000.times do |x|
+    puts "Creating post #{x}"
+    post = Post.new(
     title: "Title #{x}",
     body: "Body #{x} Words go here idk",
-    user_id: User.first.id
-  )
-
-  5.times do |y|
-    Comment.create(
-      body: "comment #{y}",
-      user_id: User.second.id,
-      post_id: post.id
-    )
+    user: Toko)
+    
+    10.times do |y|
+      puts "Creating comment #{y} for post #{x}"
+      # Build comments for the post
+      post.comments.build(
+        body: "comment #{y}",
+        user: Baka)
+      end
+      posts.push(post)
   end
+  Post.import(posts, recursive: true)
 end
+
+puts "Elapsed time is #{elapsed.real} seconds"
