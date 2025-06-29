9358ac88605086a9434b8a36ade1ef50ade9b1d1
toatoko <tokora212@gmail.com>
2025-06-30 02:43:20 +0300

    fixes test

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module BlogDemo
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    config.action_mailer.default_url_option = { host: "http://localhost:3000" }
+    config.action_mailer.default_url_options = { host: "localhost:3000" }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
