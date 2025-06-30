28ba25e9ee9448d13e754d3835120171ae22e630
toatoko <tokora212@gmail.com>
2025-06-30 03:04:03 +0300

    skip

--- test/controllers/members_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class MembersControllerTest < ActionDispatch::IntegrationTest
-  test "should get dashboard" do
-    get members_dashboard_url
-    assert_response :success
-  end
-end
