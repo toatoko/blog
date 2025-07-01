5c877530443a26ebb9b812c8cf9bbc7f8a4073ca
toatoko <tokora212@gmail.com>
2025-07-01 11:24:59 +0300

    notification fixes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   before_action :require_admin, except: %i[index show]
   # GET /projects or /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.includes([:rich_text_body]).all
   end
 
   # GET /projects/1 or /projects/1.json
