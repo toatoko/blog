4aa3c1fc9dd34cdaa59e611f74cb3350a6d8619c
toatoko <tokora212@gmail.com>
2025-07-03 04:12:24 +0300

    drag and drop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_01_082559) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_02_231111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -276,6 +276,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_01_082559) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "body"
+    t.integer "position"
   end
 
   create_table "users", force: :cascade do |t|
