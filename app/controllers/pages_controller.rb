2e969cd8616970772e3b1d689741ea6b173f23c9
toatoko <tokora212@gmail.com>
2025-07-05 04:29:50 +0300

    debugging

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
+   
   def home
+    # Rails.logger.info "Render the homepage"
     return unless current_user
     return if current_user.payment_processor.nil?
     @portal_session = current_user.payment_processor.billing_portal
